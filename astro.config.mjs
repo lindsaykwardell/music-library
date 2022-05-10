4e791153d9cf42737b6abea4421f6a052198a408
Lindsay Wardell <three060@gmail.com>
2022-05-10 08:29:52 -0700

    enable netlify SSR build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import elmPlugin from "vite-plugin-elm";
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
+  integrations: [tailwind()],
   vite: {
-    plugins: [elmPlugin.default()]
+    plugins: [elmPlugin.default()],
   },
-  integrations: [tailwind()]
 });
